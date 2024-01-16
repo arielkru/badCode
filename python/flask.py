0d829501cbf3c4e610884e43bb3f29fe649d1ce0
arielkru <63583491+arielkru@users.noreply.github.com>
2024-01-16 19:04:48 +0200

    Update flask.py

--- python/flask.py
+++ python/flask.py
@@ -4,8 +4,10 @@ import bleach
 app = Flask(__name__)
 
 # curl -X GET "http://localhost:5000/tainted7/touch%20HELLO"
+
 @app.route("/tainted7/<something>")
 def test_sources_7(something):
+	
 
     os.system(request.remote_addr)
 
@@ -14,10 +16,13 @@ def test_sources_7(something):
 @app.route("/sanitized/<something>")
 def test_sources_7(something):
     data = flask.request.args.get("key")
+	
     sanitized_data = bleach.clean(data)
     os.system(sanitized_data)
 
+	
     return "bar"
 
 if __name__ == "__main__":
 	app.run(debug=True)
+	
