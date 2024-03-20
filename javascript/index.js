64c95a3ad1fe8e3c01988a03557e701a88cea2d5
arielkru <63583491+arielkru@users.noreply.github.com>
2024-03-20 15:46:56 +0200

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -12,11 +12,14 @@ fs.chmodSync(argOne, mode)
 const mode2 = fs.constants.S_IXUSR;
 const flags = 'w'
 fs.open('temp_foo', flags, mode2, function (err, f) {
+    
     if (err) {
         return console.error(err);
     }
+    
     console.log(f);
     console.log("File opened!!");
+    
 });
 
 fs.writeFileSync("temp_programming.txt", "foo", {mode:fs.constants.S_IXUSR | fs.constants.S_IRUSR	});
@@ -25,14 +28,17 @@ new Buffer(5);
 new Buffer(res.body.size);
 
 function getVarFromObject(someVar, obj) {
+    
     obj.escapeMarkup = false;
     const someObjVar = {s: someVar}
+    
     const val = obj[someObjVar.s]
     return val
 }
 
 const expression = new String("2 + 2");
 eval(String(expression));
+
 
 
 
